ac161848a9a98c77a9ef03f2c4c39bb3a6469b6c
alberto rodriguez ruiz <alberto.1009@hotmail.com>
2019-10-29 19:37:17 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/Configuracion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes
+{
+    class Configuracion : INotifyPropertyChanged
+    {
+        private string ajusteImagen;
+        private string fondo;
+
+        public string AjusteImagen
+        {
+            get { return ajusteImagen; }
+            set
+            {
+                if(ajusteImagen != value)
+                {
+                    ajusteImagen = value;
+                    NotifyPropertyChanged("AjusteImagen");
+                }
+            }
+        }
+
+        public string Fondo
+        {
+            get { return fondo; }
+            set
+            {
+                if(fondo != value)
+                {
+                    fondo = value;
+                    NotifyPropertyChanged("Fondo");
+                }
+            }
+        }
+        
+        public Configuracion()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Superheroes/Superheroe.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes
+{
+    class Superheroe : INotifyPropertyChanged
+    {
+        private string nombre;
+        private string enemigo;
+        private string foto;
+        private bool vengador;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                if(nombre != value)
+                {
+                    nombre = value;
+                    NotifyPropertyChanged("Nombre");
+                }
+            }
+        }
+
+        public string Enemigo
+        {
+            get { return enemigo; }
+            set
+            {
+                if(enemigo != value)
+                {
+                    enemigo = value;
+                    NotifyPropertyChanged("Enemigo");
+                }
+            }
+        }
+        
+        public string Foto
+        {
+            get { return foto; }
+            set
+            {
+                if(foto != value)
+                {
+                    foto = value;
+                    NotifyPropertyChanged("Foto");
+                }
+            }
+        }
+
+        public bool Vengador
+        {
+            get { return vengador; }
+            set
+            {
+                if(vengador != value)
+                {
+                    vengador = value;
+                    NotifyPropertyChanged("Vengador");
+                }
+            }
+        }
+
+        public Superheroe()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static Superheroe GetSample()
+        {
+            Superheroe heroe = new Superheroe();
+            heroe.Nombre = "Daredevil";
+            heroe.Enemigo = "Kingping";
+            heroe.Foto = @"https://dam.smashmexico.com.mx/wp-content/uploads/2018/08/27150601/daredevilbio_portada.jpg";
+            heroe.Vengador = false;
+
+            return heroe;
+        }
+    }
+}
+
